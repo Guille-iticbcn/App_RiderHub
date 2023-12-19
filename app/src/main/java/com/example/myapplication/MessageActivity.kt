b05dbc832bb0319bb088ca2dac13a3f907d061a1
guilleee50 <04guillejm@gmail.com>
2023-12-19 18:50:19 +0100

    CREATED views

--- app/src/main/java/com/example/myapplication/MessageActivity.kt
+++ app/src/main/java/com/example/myapplication/MessageActivity.kt
@@ -1,45 +1,35 @@
 package com.example.myapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.messages)
         initEvents()
     }
 
     fun initEvents(){
-        val button = findViewById<Button>(R.id.button2)
         val buttonHome = findViewById<Button>(R.id.buttonHome)
         val buttonSearch = findViewById<Button>(R.id.buttonSearch)
         val buttonPhoto = findViewById<Button>(R.id.buttonPhoto)
         val buttonProfile = findViewById<Button>(R.id.buttonProfile)
 
-        button.setOnClickListener({
-            val i = Intent (this, MessageActivity::class.java)
-            startActivity(i)
-        })
-
         buttonHome.setOnClickListener({
             val i = Intent (this, MainActivity::class.java)
             startActivity(i)
         })
-
         buttonSearch.setOnClickListener({
             val i = Intent (this, SearchActivity::class.java)
             startActivity(i)
         })
-
         buttonPhoto.setOnClickListener({
             val i = Intent (this, NewPublicationActivity::class.java)
             startActivity(i)
         })
-
         buttonProfile.setOnClickListener({
             val i = Intent (this, ProfileActivity::class.java)
             startActivity(i)
